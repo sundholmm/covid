603f582b613acc894b2924e6e1e45184653e2fdf
sundholmm <oskar.sundholm@protonmail.com>
2020-12-14 18:52:57 +0200

    added python migration script

---
+++ utils/migrate.py
@@ -0,0 +1,15 @@
+from requests import get, post
+from pathlib import Path
+from json import load, dumps
+
+# This script downloads the covid data in JSON format from opendata.ecdc.europa.eu
+# and migrates it to the running (!) covid application
+
+records = Path('./data/records.json')
+if not records.is_file():
+    r = get('https://opendata.ecdc.europa.eu/covid19/casedistribution/json/')
+    records.write_bytes(r.content)
+
+with open(records) as data_file:
+    data = load(data_file)
+    post('http://localhost:8080/api/v1/records', data = dumps(data["records"]))
